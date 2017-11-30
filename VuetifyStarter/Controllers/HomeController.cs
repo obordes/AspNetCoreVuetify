cdf2c9ed288c6198de8349a5588f47e11c6b8374
Olivier Bordes <olivier.bordes@ersnet.org>
2017-11-30 14:04:39 +0100

    init

--- VuetifyStarter/Controllers/HomeController.cs
+++ VuetifyStarter/Controllers/HomeController.cs
@@ -1,37 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using VuetifyStarter.Models;
 
 namespace VuetifyStarter.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Error()
+		{
+			ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+			return View();
+		}
+	}
 }
